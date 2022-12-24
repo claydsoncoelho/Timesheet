0f7646e17886ca583c0f62df0e2a18332817ca2c
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-25 10:55:39 +1300

    a

---
+++ Timesheet.py
@@ -0,0 +1,13 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
