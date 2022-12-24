e33fc9f6bf28fae2562dff908190bd14fb30a4d9
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-25 12:54:18 +1300

    a

--- Timesheet.py
+++ Timesheet.py
@@ -1,13 +1,32 @@
-import streamlit
-import pandas
-import requests
-#import snowflake.connector
-from urllib.error import URLError
+import streamlit as st
+import pandas as pd
 
-streamlit.title('New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# streamlit run /Users/claydsoncoelho/Documents/GitHub/Timesheet/Timesheet.py
+
+tab1, tab2, tab3 = st.tabs(["Time Entry", "Reports", "Resources"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("Resources")
+    name = st.text_input('Name')
+    rate = st.number_input('Rate')
+
+    # Cache the dataframe so it's only loaded once
+    @st.experimental_memo
+    def load_data():
+        return pd.DataFrame(
+            {
+                "first column": [1, 2, 3, 4],
+                "second column": [10, 20, 30, 40],
+            }
+        )
+
+    df = load_data()
+    st.dataframe(df, use_container_width=True)
