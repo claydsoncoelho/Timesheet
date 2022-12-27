9b785e795009d6561faf1c95c587910f2b3cd343
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-28 07:43:35 +1300

    Update Timesheet.py

--- Timesheet.py
+++ Timesheet.py
@@ -13,12 +13,4 @@ my_data = pd.DataFrame(my_cur.fetchall())
 
 st.dataframe(my_data)
 
-with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
 my_cnx.close()
