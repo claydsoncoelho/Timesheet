8f0f113d5aa31aa2909c8e201a249828f1de251a
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-28 10:31:37 +1300

    a

--- Timesheet.py
+++ Timesheet.py
@@ -46,6 +46,6 @@ with tab3:
 
     resource_list = get_all_resources()
     resource_list.columns = ['Name', 'Rate']
-    resource_list['Rate'] = 999
+    resource_list['Rate'] = df['a'].apply(lambda x: x * 0.01)
     st.dataframe(resource_list, use_container_width=True)
 
