643145f0b4f8bc4146759751fce3a746bbfebb88
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-27 22:22:45 +1300

    a

--- Timesheet.py
+++ Timesheet.py
@@ -36,6 +36,7 @@ with tab3:
 
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     resource_list = get_all_resources(my_cnx)
+    st.write(resource_list)
 
     if name and rate:
         msg = insert_resource(my_cnx, name, rate)
