ed5f77bdb00ac69de71a6ec551c94dae9cd5ac92
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-28 09:35:55 +1300

    a

--- Timesheet.py
+++ Timesheet.py
@@ -1,11 +1,52 @@
 import streamlit as st
+import pandas as pd
+import snowflake.connector
 
-input = st.text_input("text", key="text")
+# streamlit run /Users/claydsoncoelho/Documents/GitHub/Timesheet/Timesheet.py
+
+tab1, tab2, tab3 = st.tabs(["Time Entry", "Reports", "Resources"])
 
 
-def clear_text():
-    st.session_state["text"] = ""
+def insert_resource(cnx, name, rate):
+    with cnx.cursor() as my_cur:
+        sql_cmd = "INSERT INTO DB_TIMESHEET.PUBLIC.RESOURCES VALUES('" + name + "', " + str(rate) + ")"
+        my_cur.execute(sql_cmd)
+    return name + " | " + str(rate)
 
 
-if st.button("clear text input", on_click=clear_text):
-    st.write(input)
+def get_all_resources(cnx):
+    with cnx.cursor() as my_cur:
+        sql_cmd = "SELECT * FROM DB_TIMESHEET.PUBLIC.RESOURCES"
+        my_cur.execute(sql_cmd)
+        my_data = pd.DataFrame(my_cur.fetchall())
+    return my_data
+
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("Resources")
+    name = st.text_input("Name", key="Name")
+    rate = st.number_input("Rate", key="Rate")
+
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+
+    def save_resource():
+        if name and rate:
+            msg = insert_resource(my_cnx, name, rate)
+            st.success(msg, icon="✅")
+            st.session_state["Name"] = ""
+            st.session_state["Rate"] = ""
+
+    st.button("Save resource", on_click=save_resource)
+
+    resource_list = get_all_resources(my_cnx)
+    st.dataframe(resource_list, use_container_width=True)
+
+    my_cnx.close()
