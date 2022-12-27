f299c19877ce842856b630f25b6eaf9c2e2d8504
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-27 22:52:13 +1300

    a

--- Timesheet.py
+++ Timesheet.py
@@ -23,7 +23,7 @@ def get_all_resources(cnx):
         {
             "first column": [1, 2, 3, 4],
             "second column": [10, 20, 30, 40],
-        }
+        })
     #return my_cur.fetchall()
 
 
